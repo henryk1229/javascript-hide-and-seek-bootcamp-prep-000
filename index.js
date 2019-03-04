57ea7a01269e7e356b77b2bc9d0b618c1124be7a
Henry Koehler <henryk1229@gmail.com>
2019-03-04 20:06:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector(selector) { 
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+function increaseRankBy(n) {
+  var lists = document.getElementById('app').querySelectorAll('ul.ranked-list li'); 
+  for (let i = 0; i < lists.length; i++) {
+    lists[i].innerHTML = parseInt(lists[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  var lists = document.getElementById('grand-node').querySelectorAll('div');
+  var array = [];
+  for (let i = 0; i < lists.length - 1; i++) {
+    array = lists[i].querySelector('div');
+  }
+  return array; 
+}
