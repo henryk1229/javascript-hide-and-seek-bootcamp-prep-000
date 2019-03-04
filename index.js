674118dbcd8f400c69055df9956db3a65792423f
Henry Koehler <henryk1229@gmail.com>
2019-03-04 19:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('.target');
 }
 function increaseRankBy(n) {
-  var lis = document.getElementById('app').querySelector('ul.ranked-list') 
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li') 
 }
